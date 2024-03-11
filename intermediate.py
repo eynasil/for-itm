1d8b81d275db40949204e5a31a997f3a713ea22e
anasel <anaselace14@gmail.com>
2024-03-11 13:07:20 +0800

    checked intermediate

--- intermediate.py
+++ intermediate.py
@@ -32,31 +32,6 @@ def caesar_cipher(message, shift):
 
 
 def shift_by_letter(letter, letter_shift):
-    '''Shift By Letter.
-    10 points.
-
-    Shift a letter to the right using the number equivalent of another letter.
-    The shift letter is any letter from A to Z, where A represents 0, B represents 1,
-        ..., Z represents 25.
-
-    Examples:
-    shift_by_letter("A", "A") -> "A"
-    shift_by_letter("A", "C") -> "C"
-    shift_by_letter("B", "K") -> "L"
-    shift_by_letter(" ", _) -> " "
-
-    Parameters
-    ----------
-    letter: str
-        a single uppercase English letter, or a space.
-    letter_shift: str
-        a single uppercase English letter.
-
-    Returns
-    -------
-    str
-        the letter, shifted appropriately.
-    '''
     letters = (
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     )
@@ -84,84 +59,29 @@ def vigenere_cipher(message, key):
 
 
 def scytale_cipher(message, shift):
-    '''Scytale Cipher.
-    15 points.
+    encoded_message = ""
+    if len(message) % shift != 0:
+        message += '_' * (shift - (len(message) % shift))
 
-    Encrypts a message by simulating a scytale cipher.
+    for i in range(len(message)):
+        index = (i // shift) + (len(message) // shift) * (i % shift)
+        encoded_message += message[index]
 
-    A scytale is a cylinder around which you can wrap a long strip of
-        parchment that contained a string of apparent gibberish. The parchment,
-        when read using the scytale, would reveal a message due to every nth
-        letter now appearing next to each other, revealing a proper message.
-    This encryption method is obsolete and should never be used to encrypt
-        data in production settings.
+    return encoded_message
 
-    You may read more about the method here:
-        https://en.wikipedia.org/wiki/Scytale
-
-    You may follow this algorithm to implement a scytale-style cipher:
-    1. Take a message to be encoded and a "shift" number.
-        For this example, we will use "INFORMATION_AGE" as
-        the message and 3 as the shift.
-    2. Check if the length of the message is a multiple of
-        the shift. If it is not, add additional underscores
-        to the end of the message until it is.
-        In this example, "INFORMATION_AGE" is already a multiple of 3,
-        so we will leave it alone.
-    3. This is the tricky part. Construct the encoded message.
-        For each index i in the encoded message, use the character at the index
-        (i // shift) + (len(message) // shift) * (i % shift) of the raw message.
-        If this number doesn't make sense, you can play around with the cipher at
-         https://dencode.com/en/cipher/scytale to try to understand it.
-    4. Return the encoded message. In this case,
-        the output should be "IMNNA_FTAOIGROE".
-
-    Example:
-    scytale_cipher("INFORMATION_AGE", 3) -> "IMNNA_FTAOIGROE"
-    scytale_cipher("INFORMATION_AGE", 4) -> "IRIANMOGFANEOT__"
-    scytale_cipher("ALGORITHMS_ARE_IMPORTANT", 8) -> "AOTSRIOALRH_EMRNGIMA_PTT"
-
-    Parameters
-    ----------
-    message: str
-        a string of uppercase English letters and underscores (underscores represent spaces)
-    shift: int
-        a positive int that does not exceed the length of message
-
-    Returns
-    -------
-    str
-        the encoded message
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
 
 def scytale_decipher(message, shift):
-    '''Scytale De-cipher.
-    15 points.
+    rows = len(message) // shift
+    cols = (len(message) // rows) + (1 if len(message) % rows != 0 else 0)
 
-    Decrypts a message that was originally encrypted with the `scytale_cipher` function above.
+    decoded_message = [""] * len(message)
 
-    Example:
-    scytale_decipher("IMNNA_FTAOIGROE", 3) -> "INFORMATION_AGE"
-    scytale_decipher("AOTSRIOALRH_EMRNGIMA_PTT", 8) -> "ALGORITHMS_ARE_IMPORTANT"
-    scytale_decipher("IRIANMOGFANEOT__", 4) -> "INFORMATION_AGE_"
+    k = 0
+    for j in range(cols):
+        for i in range(rows):
+            index = i * cols + j
+            if index < len(message):
+                decoded_message[k] = message[index]
+                k += 1
 
-    There is no further brief for this number.
-
-    Parameters
-    ----------
-    message: str
-        a string of uppercase English letters and underscores (underscores represent spaces)
-    shift: int
-        a positive int that does not exceed the length of message
-
-    Returns
-    -------
-    str
-        the decoded message
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    return "".join(decoded_message)
